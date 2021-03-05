be3930378712f0325d0b20b7404346c5be65c05c
ryanpetersonOF <ryan@openfin.co>
2021-03-05 15:51:46 +0000

    update template

--- src/Apps/Index/Index.tsx
+++ src/Apps/Index/Index.tsx
@@ -8,12 +8,7 @@ const Index: React.FC = () => {
     return (
         <Provider store={store}>
             <div style={{ padding: '12px' }}>
-                <h2>React Portal Child Windows Demo</h2>
-                <p>This demo showcases the rough functionality of rendering into a child window. The portal window is able to be updated with its own local state, passed state (props), redux provider state, attached events (onClick, etc), as well as adding or removing any children just as a normal react component would. It's best to think of the Portal component, which is the wrapper component and creator of the portal window, as a normal component. However, instead of rendering its children to the local DOM, it just ships it out to a remote DOM.</p>
-                <p>Since this works under the premise of a normal component, the portal windows can be opened and closed simply by adding or removing the portal components the react tree. Further logic could be added to hide or show or anything else, but is not part of this demo.</p>
-                <p>I have also included the renderComponentInWindow util which allows you to create a 'detached' portal window without having to render into the react tree. This is great for one off windows, where there maybe not be a lot data flowing, or you may want to control the fin window separately outside of mounting and unmounting. It should be noted that out of the box, props will be not be updated automaticaly in the detached window. Local state, redux, and prop actions (such as de/incrementing in this demo) will continue to work. If props do need to be updated, the util would simply need to be called again with the updated props and same target window. This does not cause a remount either!</p>
-                <p>This is the pattern we have used in the Notification Center and in Suite. In fact, every window you see in both of those projects is being rendered in the Portal Window fashion!</p>
-                <p>This demo does not have safe guards included for HMR or app reloads. However, this can all be solvable with the proper react hot reloading, not using randomly named windows, and proper eventing.</p>
+                <h2>Template</h2>
                 <hr />
                 <Controller />
             </div>
